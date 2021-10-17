e12c990607867aba563d2e1c94c502904077d89e
Souvik Sen <souvik22sen@gmail.com>
2021-10-17 14:45:01 +0530

    Modified chats.js and added .env file

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 import { auth } from '../firebase';
 
 import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
 
 const Chats = () => {
     const history = useHistory();
     const { user } = useAuth();
-
-    console.log(user);
+    const [loading, setLoading] = useState(true);
 
     const handleLogout = async () => {
         await auth.signOut();
@@ -17,13 +17,49 @@ const Chats = () => {
         history.push('/');
     }
 
-    // useEffect(() => {
-    //     if(!user) {
-    //         history.push('/');
+    const getFile = async (url) => {
+        const response = await fetch(url);
+        const data = await response.blob();// blobs are usually any files like images or any other type of file that you want to transfer over in binary format.
+        
+        return new File([data], "userPhoto.jpg", { type: 'image/jpeg' })
+    }
 
-    //         return;
-    //     }
-    // }, [user, history]);
+    useEffect(() => {
+        if(!user) {
+            history.push('/');
+
+            return;
+        }
+        axios.get('https://api.chatengine.io/users/me', {
+            headers: {
+                "project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
+                "user-name": user.email,
+                "user-secret": user.uid,
+            }
+        })
+        .then(() => {
+            setLoading(false);
+        })
+        .catch(() => {
+            let formdata = new FormData();
+            formdata.append('email', user.email);
+            formdata.append('username', user.email);
+            formdata.append('secret', user.uid);
+
+            getFile(user.photoURL)
+                .then((avatar) => {
+                    formdata.append('avatar', avatar, avatar.name);
+                    axios.post('https://api.chatengine.io/users/', 
+                        formdata,
+                        { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY } }
+                    )
+                    .then(() => setLoading(false))
+                    .catch((error) => console.log(error))
+                })
+        })
+    }, [user, history]);
+
+    if(!user || loading) return 'Loading... ';
 
     return (
         <div className="chats-page">
@@ -38,9 +74,9 @@ const Chats = () => {
 
             <ChatEngine
                 height="calc(100vh - 66px)"
-                projectId="4d2b9049-b794-454e-bd18-a1a76166aa28"
-                userName="."
-                userSecret="."
+                projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+                userName={user.email}
+                userSecret={user.uid}
             />
         </div>
     );
